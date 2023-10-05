f78445adc297691f270555c23ee42ec3b7485ee6
zlorgoncho1 <papamatardiop3@gmail.com>
2023-10-05 00:04:58 +0000

    Cleaning the branch

--- iac/scripts/cluster/kubeconfig.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p /home/ubuntu/.kube
-sudo cp -i /home/ubuntu/scripts/config /home/ubuntu/.kube/config
-sudo chown $(id -u):$(id -g) /home/ubuntu/.kube/config
--- iac/scripts/deskspace/kubectl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-args="$@"
-sudo docker compose --file='/home/ubuntu/pipelineboy/iac/docker-compose.iac.yml' run --rm kubectl $args
